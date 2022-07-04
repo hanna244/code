570b79d07866a0e34d8ab70926b3ebaf40708894
hanna244 <shhn0509@gmail.com>
2022-07-04 23:23:21 +0900

    add: 1강 JSX 환경 세팅

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
